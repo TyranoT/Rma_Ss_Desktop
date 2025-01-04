600867b6fe92458133cea43dc2fa32ae44aa3871
Italo Git <sulamita.italo12@gmail.com>
2025-01-03 23:52:41 -0300

    add tailwind

--- rma_ss_desktop/electron.vite.config.ts
+++ rma_ss_desktop/electron.vite.config.ts
@@ -4,15 +4,27 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        "@/lib": resolve('src/main/lib'),
+        "@shared": resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: "src/renderer/assets/**",
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/pages': resolve('src/renderer/src/pages'),
       }
     },
     plugins: [react()]
