6a34e965cbfa6ea7ddca244ad68b56b6c51e355d
Italo Git <sulamita.italo12@gmail.com>
2025-01-04 01:12:11 -0300

    TAILWIND

--- rma_ss_desktop/tailwind.config.js
+++ rma_ss_desktop/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require('@nextui-org/react')
 module.exports = {
-  content: [],
+  content: [
+    './src/renderer/**/*.{js,jsx,ts,tsx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
+  darkMode: 'class'
 }
 
